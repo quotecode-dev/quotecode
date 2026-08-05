32c09813429cd3b0bd6fe58e4d69f073b036664c
Shlomo <shlomisiny@gmail.com>
2026-08-06 02:14:38 +0300

    Fix inline RTL/LTR text ordering for email field in PublicQuote header

--- src/pages/PublicQuote.jsx
+++ src/pages/PublicQuote.jsx
@@ -213,7 +213,7 @@ export default function PublicQuote() {
                   <div style={{ color: '#64748b', fontSize: '0.82rem', lineHeight: '1.4' }}>
                     {bizTaxId && <div>ח.פ / עוסק: <span dir="ltr" style={{ display: 'inline-block' }}>{bizTaxId}</span></div>}
                     {bizPhone && <div>טלפון: <span dir="ltr" style={{ display: 'inline-block' }}>{bizPhone}</span></div>}
-                    {bizEmail && <div>אימייל: <span dir="ltr" style={{ display: 'inline-block' }}>{bizEmail}</span></div>}
+                    {bizEmail && <div dir="ltr" style={{ textAlign: 'right' }}><span>{bizEmail}</span> :אימייל</div>}
                     {bizAddress && <div>כתובת: {bizAddress}</div>}
                   </div>
                 </div>
