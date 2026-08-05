6badea066a215316b289a60448f17b05600671f7
Shlomo <shlomisiny@gmail.com>
2026-08-05 17:29:42 +0300

    Update quote terms for private clients and apply international 0% VAT rule

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import LandingGlobal from './pages/LandingGlobal';
 import LandingLocal from './pages/LandingLocal';
